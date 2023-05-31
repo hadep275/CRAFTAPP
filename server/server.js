69ccb1b77af1f83ed5664d69fc9051b9f79af4ad
hadep275 <hadep275@gmail.com>
2023-05-31 10:55:55 -0600

    commit changes

--- server/server.js
+++ server/server.js
@@ -3,19 +3,24 @@ import { inventoryRouter } from "./routes/inventory.js";
 import { userRouter } from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 import {contactRouter} from "./routes/contactRoutes.js"
+import path from 'path'
 
 dotenv.config();
 const app = express();
 const port = 6660;
+const __dirname = path.resolve()
+const pathToBuild = path.join(__dirname, '../frontend/build')
 
 app.use(express.json());
-
 app.use("/api/user", userRouter);
 app.use("/api/inventory", inventoryRouter);
 app.use('/api/contact', contactRouter);
+app.use(express.static(pathToBuild));
 
 app.listen(port, (response, request) => {
   console.log(`To Craft is to life: ${port}`);
 });
+
+
 
 
